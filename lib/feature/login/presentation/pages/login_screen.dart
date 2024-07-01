e835db0dfa9faaeefff7b03fcecc866cb30bef49
rimavebri <rmvbry@gmail.com>
2024-07-01 14:02:37 +0700

    chore: minor update

--- lib/feature/login/presentation/pages/login_screen.dart
+++ lib/feature/login/presentation/pages/login_screen.dart
@@ -2,9 +2,7 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
