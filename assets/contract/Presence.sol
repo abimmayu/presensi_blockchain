62bfe7339cedd95b748c490eee17694f1c37d35a
rimavebri <rmvbry@gmail.com>
2024-12-29 08:24:00 +0700

    🐞 fix: smart contract enhancement

--- assets/contract/Presence.sol
+++ assets/contract/Presence.sol
@@ -7,42 +7,41 @@ contract Presence {
         uint256 nip;
     }
 
-    mapping(uint256 => Present[]) objectPresent;
-    uint256[] listOfPresent;
+    mapping(uint256 => Present[]) private objectPresent;
+    uint256[] private listOfPresent;
 
-    // Input Present
     function inputPresent(uint256 presentTime, uint256 employee_nip) public {
         objectPresent[presentTime].push(Present(block.timestamp, employee_nip));
-        listOfPresent.push(presentTime);
+        if (objectPresent[presentTime].length == 1) {
+            listOfPresent.push(presentTime);
+        }
     }
 
-    // Mengembalikan daftar waktu presensi
     function getPresentTimes() public view returns (uint256[] memory) {
         return listOfPresent;
     }
 
-    // Mengembalikan presensi berdasarkan waktu tertentu
-    function getPresentByTime(uint256 time) public view returns (Present[] memory) {
+    function getPresentByTime(
+        uint256 time
+    ) public view returns (Present[] memory) {
         return objectPresent[time];
     }
 
-    // Mengembalikan presensi dalam bentuk paginated
-    function getPaginatedPresents(uint256 startIndex, uint256 endIndex) public view returns (Present[] memory) {
+    function getPaginatedPresents(
+        uint256 startIndex,
+        uint256 endIndex
+    ) public view returns (Present[] memory) {
         require(startIndex < endIndex, "Invalid indices");
         require(endIndex <= listOfPresent.length, "End index out of range");
 
         uint256 totalPresentsCount = 0;
-
-        // Hitung jumlah total present dalam rentang
         for (uint256 i = startIndex; i < endIndex; i++) {
             totalPresentsCount += objectPresent[listOfPresent[i]].length;
         }
 
-        // Buat array untuk menyimpan hasil paginated
         Present[] memory paginatedPresents = new Present[](totalPresentsCount);
         uint256 currentIndex = 0;
 
-        // Isi array hasil paginated
         for (uint256 i = startIndex; i < endIndex; i++) {
             uint256 time = listOfPresent[i];
             Present[] memory presentsForTime = objectPresent[time];
